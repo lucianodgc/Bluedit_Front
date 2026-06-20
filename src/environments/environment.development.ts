a40aef81db73ab34b8ccbf6bb02a9fa31f3fac21
lucianodgc <luciano.digiovanni@estudiantes.utec.edu.uy>
2026-06-20 16:43:06 -0300

    Correccion eliminar post

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
     apiUrl: 'http://localhost/Inicio/Bluedit/api',
     avatarUrl: 'https://api.dicebear.com/9.x/initials/svg?seed=',
     serverUrl: 'http://localhost/Inicio/Bluedit/',
-    version: "0.6.0",
+    version: "0.7.1",
     production: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     apiUrl: 'http://192.168.1.127/Inicio/Bluedit/api',
     avatarUrl: 'https://api.dicebear.com/7.x/initials/svg?seed=',
     serverUrl: 'http://192.168.1.127/Inicio/Bluedit/',
-    version: "0.6.0",
+    version: "0.7.1",
     production: true
 };
