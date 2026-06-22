9c3eb5144a8615248631adfa1730b7283fb9c659
lucianodgc <luciano.digiovanni@estudiantes.utec.edu.uy>
2026-06-22 09:21:58 -0300

    Delete comment hecho

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
     apiUrl: 'http://localhost/Inicio/Bluedit/api',
     avatarUrl: 'https://api.dicebear.com/9.x/initials/svg?seed=',
     serverUrl: 'http://localhost/Inicio/Bluedit/',
-    version: "0.7.1",
+    version: "1.0.0",
     production: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     apiUrl: 'http://192.168.1.127/Inicio/Bluedit/api',
     avatarUrl: 'https://api.dicebear.com/7.x/initials/svg?seed=',
     serverUrl: 'http://192.168.1.127/Inicio/Bluedit/',
-    version: "0.7.1",
+    version: "1.0.0",
     production: true
 };
